19ae07106f2517dad66031d5b194414435223cb7
Jieruei Chang <30610197+knosmos@users.noreply.github.com>
2022-11-04 08:43:45 -0400

    Update distribute.py

--- distribute.py
+++ distribute.py
@@ -35,7 +35,8 @@ def distribute(entries, num_judges, entries_per_judge = 5):
             print("Graph connectivity test passed")
         repeated = False
         for i in res:
-            if len(set(i)) != len(i):
+            k = [j[0] for j in res[i]]
+            if len(set(k)) != len(k):
                 repeated = True
         if repeated:
             print("Repeating assignments detected, trying again")
